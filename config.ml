5732a95886efc5d404ab5a013eb0448658b96da0
Matt Gray <matthew.thomas.gray@gmail.com>
2015-11-08 12:13:56 +0000

    CLOCK -> PCLOCK

--- config.ml
+++ config.ml
@@ -1,12 +1,13 @@
 open Mirage
 
-let main = foreign "Unikernel.Main" (console @-> clock @-> job)
-
+let main =
+  let libraries = ["ptime"] in
+  let packages =  ["ptime"] in
+  foreign
+    ~libraries ~packages
+    "Unikernel.Main" (console @-> clock @-> pclock @-> job)
 
 let () =
-  add_to_opam_packages ["ptime"];
-  add_to_ocamlfind_libraries ["ptime"];
-
   register "console" [
-    main $ default_console $ default_clock
+    main $ default_console $ default_clock $ default_posix_clock
   ]
--- unikernel.ml
+++ unikernel.ml
@@ -1,6 +1,6 @@
 open Lwt
 
-module Main (C: V1_LWT.CONSOLE) (CLOCK: V1.CLOCK) = struct
+module Main (C: V1_LWT.CONSOLE) (CLOCK: V1.CLOCK) (PCLOCK: V1.PCLOCK) = struct
 
   let get_time_string_old_api = fun () ->
     CLOCK.time () |>
@@ -10,26 +10,26 @@ module Main (C: V1_LWT.CONSOLE) (CLOCK: V1.CLOCK) = struct
       | None -> failwith "Invalid span from CLOCK.time"
 
   let get_time_string_new_api = fun () ->
-    CLOCK.now_d_ps () |>
+    PCLOCK.now_d_ps () |>
     Ptime.Span.of_d_ps |>
     Ptime.of_span |>
     function
       | Some time -> Ptime.to_rfc3339 ~frac:3 time
-      | None -> failwith "Invalid span from CLOCK.time"
+      | None -> failwith "Invalid span from PCLOCK.now_d_ps"
 
-  let start console clock =
+  let start console clock pclock =
     let log = C.log console in
     for_lwt i = 0 to 4 do
       lwt () = OS.Time.sleep 1.0 in
       log (Printf.sprintf "the utc time from CLOCK.time is: %s"
         (get_time_string_old_api ()));
-      log (Printf.sprintf "the utc time from CLOCK.now_d_ps is: %s"
+      log (Printf.sprintf "the utc time from PCLOCK.now_d_ps is: %s"
         (get_time_string_new_api ()));
-      log (Printf.sprintf "the local time offset from UTC (CLOCK.current_tz_offset_s) is: %d"
-        (CLOCK.current_tz_offset_s ()));
-      match CLOCK.period_d_ps () with
+      log (Printf.sprintf "the local time offset from UTC (PCLOCK.current_tz_offset_s) is: %d"
+        (PCLOCK.current_tz_offset_s ()));
+      match PCLOCK.period_d_ps () with
         | Some (d, ps) -> return (
-          log ( Printf.sprintf "the period of the clock (CLOCK.period_d_ps) is: %d days %Ld ps" d ps))
+          log ( Printf.sprintf "the period of the clock (PCLOCK.period_d_ps) is: %d days %Ld ps" d ps))
         | None -> return (log "Clock period unavailable")
     done
 end
